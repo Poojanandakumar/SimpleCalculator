e1b62a9f64af74e5a0331ef271b132b85eb0543a
Pooja Nandakumar <poojanandakumar34@gmail.com>
2020-10-31 17:36:09 +0530

    written main
    assigned variable
    written function for addition operation

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.simplecalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText1, editText2;
+    Button addButton;
+    TextView result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText1 = findViewById(R.id.editText1);
+        editText2 = findViewById(R.id.editText2);
+        addButton = findViewById(R.id.add_button);
+        result = findViewById(R.id.answerView);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              result.setText( editText1.getText().toString() + editText2.getText().toString());
+
+            }
+        });
+
+
     }
 }
